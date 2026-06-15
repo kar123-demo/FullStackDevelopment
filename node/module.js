29da4bdc47317d3c5399047fb1a9f7bc791dc508
karthiklaxman <karthiklaxman21@gmail.com>
2026-06-15 15:11:41 +0530

    servers

--- node/module.js
+++ node/module.js
@@ -13,12 +13,14 @@ console.log(fullPath);
 
 
 
-// const os=require('os')
-// console.log(`OS Platform: ${os.platform()}`);
-// console.log(`OS Type: ${os.type()}`);
-// console.log(`OS Release: ${os.release()}`);
-// console.log(`CPU Architecture: ${os.arch()}`);
-// console.log(`Hostname: ${os.hostname()}`);
-// const totalMemGB = (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2);
-// const freeMemGB = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
-// console.log(`Memory: ${freeMemGB}GB free of ${totalMemGB}GB`);
+const os=require('os')
+console.log(`OS Platform: ${os.platform()}`);
+console.log(`OS Type: ${os.type()}`);
+console.log(`OS Release: ${os.release()}`);
+console.log(`CPU Architecture: ${os.arch()}`);
+console.log(`Hostname: ${os.hostname()}`);
+const totalMemGB = (os.totalmem() )
+const freeMemGB = (os.freemem())
+console.log(`Memory: ${freeMemGB} free of ${totalMemGB}`);
+
+
